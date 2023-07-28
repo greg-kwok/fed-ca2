26f380bb2f2d65bdeea6ebb5ef64c99aa8c06a01
greg-kwok <134683399+greg-kwok@users.noreply.github.com>
2023-07-28 20:09:25 +0800

    Add files via upload

---
+++ js/flipcard.js
@@ -0,0 +1,5 @@
+const card=document.getElementById("card")
+card.addEventListener("click",flipcard);
+function flipcard(){
+    card.classList.toggle("flipcard")
+}
---
+++ js/game.js
@@ -0,0 +1,57 @@
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
+var newestcount=0
+hole.addEventListener('animationiteration', () => {
+    var random = -((Math.random()*300)+150);
+    hole.style.top = random + "px";
+    counter++;
+});
+setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jumping==0){
+        character.style.top = (characterTop+3)+"px";
+    }
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var cTop = -(500-characterTop);
+    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
+        alert("Game over. Score: "+(counter-1));
+        character.style.top = 100 + "px";
+        if((counter-1)>newestcount){
+            newestcount=(counter-1)
+        }
+        document.getElementById("scoresheet").innerHTML = ("Highest Score: "+(newestcount));
+        counter=0;
+    }
+},10);
+
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&&(jumpCount<15)){
+            character.style.top = (characterTop-4)+"px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
+}
+
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+      jump()
+    }
+  })
+document.addEventListener("click", function(){
+    jump()
+});
+
+document.getElementById("scoresheet").innerHTML = ("Highest Score: "+(newestcount));
